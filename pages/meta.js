bb1298224fe97bbbcfcba94d407f69db970b2a24
virgknight <virginiaknight888@gmail.com>
2022-02-14 15:16:26 -0700

    Please work omg

--- pages/meta.js
+++ pages/meta.js
@@ -7,7 +7,7 @@ export default function Meta() {
             <title>Happy Valentine&apos;s Day my love! ❤️</title>
             <link rel="icon" href="/favicon.ico" />
             <link rel="preconnect" href="https://fonts.googleapis.com"/>
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
             <link href="https://fonts.googleapis.com/css2?family=Baloo+2:wght@700&family=Chicle&family=Cinzel&family=Londrina+Outline&family=Pacifico&display=swap" rel="stylesheet"/>
         </Head>
     )
